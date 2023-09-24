80ad7cb3facb7da6f1b3cc79df1aad64aed28db5
Matias Zapf <mzapf@pm.me>
2023-09-24 18:47:13 -0300

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react"
 import { FaGithub, FaLinkedin } from "react-icons/fa"
 
 const links = [
@@ -12,20 +13,34 @@ const links = [
 ]
 
 const Footer = () => {
+    const [fechaHora, setFechaHora] = useState("")
+
+    useEffect(() => {
+        const now = new Date()
+        const fecha = now.getDate()
+        const mes = now.getMonth() + 1
+        const año = now.getFullYear()
+        const hora = now.getHours()
+        const minuto = now.getMinutes()
+
+        setFechaHora(`${hora}:${minuto} ${fecha}/${mes}/${año}`)
+    }, [])
+
     return (
         <footer className="w-full flex items-center absolute bottom-0 border-2 border-gray-800">
-            <p className="p-2">find me in:</p>
+            <p className="flex p-2 border-r-2 border-gray-800">find me in:</p>
             {links.map((link, index) => (
                 <a
                     key={index}
                     href={link.href}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="hover:text-amber-400 text-4xl p-2"
+                    className="hover:text-amber-400 text-2xl p-2 border-r-2 border-gray-800"
                 >
                     {link.logo}
                 </a>
             ))}
+            <p className="flex grow justify-end pr-2">{fechaHora}</p>
         </footer>
     )
 }
