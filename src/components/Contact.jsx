e132b514b5fc5ebee12dc105fd42aaa87dfc3021
Matias Zapf <mzapf@pm.me>
2023-10-02 18:41:09 -0300

    add a message to Contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,21 +1,24 @@
-import React, { useRef } from "react"
+import React, { useRef, useState } from "react"
 import emailjs from "@emailjs/browser"
-
-const SERVICE_ID = import.meta.env.VITE_SERVICE_ID
-const TEMPLATE_ID = import.meta.env.VITE_TEMPLATE_ID
-const PUBLIC_KEY = import.meta.env.VITE_PUBLIC_KEY
 
 const Contact = () => {
     const form = useRef()
+    const [showThankYou, setShowThankYou] = useState(true)
 
     const sendEmail = (e) => {
         e.preventDefault()
 
         emailjs
-            .sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY)
+            .sendForm(
+                import.meta.env.VITE_SERVICE_ID,
+                import.meta.env.VITE_TEMPLATE_ID,
+                form.current,
+                import.meta.env.VITE_PUBLIC_KEY
+            )
             .then(
                 (result) => {
-                    console.log(result.text)
+                    e.target.reset()
+                    setShowThankYou(true)
                 },
                 (error) => {
                     console.log(error.text)
@@ -23,35 +26,45 @@ const Contact = () => {
             )
     }
 
+    const ThankYou = () => {
+        return <h1 className="text-4xl">Thank you for contacting me!</h1>
+    }
+
+    const ContactForm = () => {
+        return (
+            <form ref={form} onSubmit={sendEmail} className="flex flex-col">
+                <label>Name</label>
+                <input
+                    type="text"
+                    name="user_name"
+                    required
+                    placeholder="Your name"
+                    className="text-black"
+                />
+                <label>Email</label>
+                <input
+                    type="email"
+                    name="user_email"
+                    required
+                    placeholder="Your email"
+                    className="text-black"
+                />
+                <label>Message</label>
+                <textarea
+                    name="message"
+                    required
+                    placeholder="Your message"
+                    className="text-black"
+                />
+                <input type="submit" value="Send" />
+            </form>
+        )
+    }
+
     return (
         <>
             <div className="h-[85vh] flex items-center justify-evenly">
-                <form ref={form} onSubmit={sendEmail} className="flex flex-col">
-                    <label>Name</label>
-                    <input
-                        type="text"
-                        name="user_name"
-                        required
-                        placeholder="Your name"
-                        className="text-black"
-                    />
-                    <label>Email</label>
-                    <input
-                        type="email"
-                        name="user_email"
-                        required
-                        placeholder="Your email"
-                        className="text-black"
-                    />
-                    <label>Message</label>
-                    <textarea
-                        name="message"
-                        required
-                        placeholder="Your message"
-                        className="text-black"
-                    />
-                    <input type="submit" value="Send" />
-                </form>
+                {showThankYou ? <ThankYou /> : <ContactForm />}
             </div>
         </>
     )
