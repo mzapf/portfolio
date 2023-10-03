1edf771afb47b6a4c43a0a807ca5cd1c937c4370
Matias Zapf <mzapf@pm.me>
2023-10-02 23:16:31 -0300

    update Contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import emailjs from "@emailjs/browser"
 
 const Contact = () => {
     const form = useRef()
-    const [showThankYou, setShowThankYou] = useState(true)
+    const [showThankYou, setShowThankYou] = useState(false)
 
     const sendEmail = (e) => {
         e.preventDefault()
@@ -27,37 +27,58 @@ const Contact = () => {
     }
 
     const ThankYou = () => {
-        return <h1 className="text-4xl">Thank you for contacting me!</h1>
+        return (
+            <h1 className="text-6xl text-amber-400 animate-pulse">
+                Thank you for contacting me!
+            </h1>
+        )
     }
 
     const ContactForm = () => {
         return (
-            <form ref={form} onSubmit={sendEmail} className="flex flex-col">
-                <label>Name</label>
-                <input
-                    type="text"
-                    name="user_name"
-                    required
-                    placeholder="Your name"
-                    className="text-black"
-                />
-                <label>Email</label>
-                <input
-                    type="email"
-                    name="user_email"
-                    required
-                    placeholder="Your email"
-                    className="text-black"
-                />
-                <label>Message</label>
-                <textarea
-                    name="message"
-                    required
-                    placeholder="Your message"
-                    className="text-black"
-                />
-                <input type="submit" value="Send" />
-            </form>
+            <div className="w-80 border-2 border-gray-800 rounded-xl p-4 shadow-lg shadow- shadow-gray-800">
+                <div className="flex justify-center">
+                    <h1 className="text-amber-400 pb-4 font-bold text-2xl">
+                        Contact Me
+                    </h1>
+                </div>
+                <div className="flex flex-col gap-4 bg-custom-gray-2 border-2 border-black rounded-xl py-4">
+                    <form
+                        ref={form}
+                        onSubmit={sendEmail}
+                        className="flex flex-col gap-2 px-4"
+                    >
+                        <label>Name</label>
+                        <input
+                            type="text"
+                            name="user_name"
+                            required
+                            placeholder="Your name"
+                            className="rounded py-2 px-3 text-gray-700"
+                        />
+                        <label>Email</label>
+                        <input
+                            type="email"
+                            name="user_email"
+                            required
+                            placeholder="Your email"
+                            className="rounded py-2 px-3 text-gray-700"
+                        />
+                        <label>Message</label>
+                        <textarea
+                            name="message"
+                            required
+                            placeholder="Your message"
+                            className="rounded py-2 px-3 text-gray-700"
+                        />
+                        <input
+                            type="submit"
+                            value="Send"
+                            className="mt-2 py-2 rounded bg-custom-gold text-custom-gray-2 hover:bg-amber-400"
+                        />
+                    </form>
+                </div>
+            </div>
         )
     }
 
