e945c310c6dd64b9b29d73b62c4e8497c4366a89
Matias Zapf <mzapf@pm.me>
2023-10-03 20:06:05 -0300

    add CodeEditor stuff

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,21 @@
 import { CodeEditor } from "./CodeEditor"
 
 const About = () => {
+    const content = [
+        "About me",
+        "Learning to develop modern web applications that involve data analytics.",
+        "I'm currently learning React, Node.js, and MongoDB.",
+        "I'm looking to collaborate on projects that involve data analytics.",
+    ]
+
+    const commented_content = `/**
+  * ${content.join("\n  * ")}
+  */`
+
     return (
-        <>
-            <CodeEditor />
-        </>
+        <div className="p-4">
+            <CodeEditor content={commented_content} />
+        </div>
     )
 }
 
--- src/components/CodeEditor.jsx
+++ src/components/CodeEditor.jsx
@@ -1,5 +1,19 @@
-const CodeEditor = () => {
-    return <></>
+const CodeEditor = ({ content }) => {
+    const lines = content.split("\n")
+
+    return (
+        <div className="border rounded p-2 bg-gray-800 text-white relative overflow-x-scroll">
+            <div className="absolute top-0 left-0 p-2">
+                {lines.map((_, index) => (
+                    <div key={index} className="text-gray-500">
+                        {index + 1}
+                    </div>
+                ))}
+            </div>
+
+            <div className="pl-8 whitespace-pre-wrap">{content}</div>
+        </div>
+    )
 }
 
 export { CodeEditor }
